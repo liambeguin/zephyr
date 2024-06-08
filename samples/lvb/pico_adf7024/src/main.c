b417d2e48e77754bb4f0be6895b32a2577baed98
Liam Beguin <liambeguin@gmail.com>
2024-06-08 15:41:06 -0400

    lvb: pico_adf7024: update after rewiring setup
    The setup now uses 2 rpi_pico boards, one to flash one for hw.
    also:
    - no longer rerouting spi, just using defaut pins for SPI0
    - programming over SWD, not DFU like thing
    - no longer using cdc-acm for shell, switched to uart0 routed through
      the pico_probe
    - fixed SPI interface to the ADF7024
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- samples/lvb/pico_adf7024/src/main.c
+++ samples/lvb/pico_adf7024/src/main.c
@@ -13,11 +13,10 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 // Devicetree
 #define CONSOLE_DEVICE DEVICE_DT_GET(DT_CHOSEN(zephyr_console))
-#define USB_CDC_ACM_CHECK DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart)
+
 
 int main(void)
 {
-#if (IS_ENABLED(USB_CDC_ACM_CHECK) && IS_ENABLED(CONFIG_LOG))
 	const struct device *const console_dev = CONSOLE_DEVICE;
 	uint32_t dtr_line = 0;
 
@@ -25,7 +24,6 @@ int main(void)
 		uart_line_ctrl_get(console_dev, UART_LINE_CTRL_DTR, &dtr_line);
 		k_sleep(K_MSEC(100));
 	}
-#endif
 
 	return 0;
 }
