e827df72e634a58ae6dc080635eab83b1bfe55e2
Liam Beguin <liambeguin@gmail.com>
2024-11-25 18:16:04 -0500

    ieee802154: adf702x: start shell driver
    Move shell commands to the driver so we can reuse these across apps.
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

---
+++ drivers/ieee802154/ieee802154_adf702x_shell.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <zephyr/net_buf.h>
+#include <zephyr/net/ieee802154_radio.h>
+#include <zephyr/shell/shell.h>
+
+#include "ieee802154_adf702x.h"
+
+#define ADF702X_ARGV_DEV      (1)
+#define ADF702X_ARGV_CW_MODE  (2)
+#define ADF702X_ARGV_TX_FIRST (2)
+#define ADF702X_MAX_TXLEN     (100)
+
+#define ADF702X_LIST_ENTRY(node_id)                                                                \
+	{                                                                                          \
+		.dev = DEVICE_DT_GET(node_id),                                                     \
+	},
+
+#define IS_ADF702X_NODE(node_id)                                                                   \
+	COND_CODE_1(DT_NODE_HAS_COMPAT(node_id, adi_adf702x), (ADF702X_LIST_ENTRY(node_id)), ())
+
+static struct adf702x_inst {
+	const struct device *dev;
+} adf702x_list[] = {DT_FOREACH_STATUS_OKAY_NODE(IS_ADF702X_NODE)};
+
+static void get_adf702x_comp(size_t idx, struct shell_static_entry *entry)
+{
+	if (idx < ARRAY_SIZE(adf702x_list)) {
+		entry->syntax = adf702x_list[idx].dev->name;
+		entry->handler = NULL;
+		entry->subcmd = NULL;
+		entry->help = "ADF702x Device";
+	} else {
+		entry->syntax = NULL;
+	}
+}
+SHELL_DYNAMIC_CMD_CREATE(dsub_adf702x, get_adf702x_comp);
+
+static struct adf702x_inst *get_adf702x(const char *device_label)
+{
+	for (int i = 0; i < ARRAY_SIZE(adf702x_list); i++) {
+		if (!strcmp(device_label, adf702x_list[i].dev->name)) {
+			return &adf702x_list[i];
+		}
+	}
+
+	/* This will never happen because was prompted by shell */
+	__ASSERT_NO_MSG(false);
+
+	return NULL;
+}
+
+static int cmd_adf702x_status(const struct shell *sh, size_t argc, char **argv)
+{
+	const struct device *dev = get_adf702x(argv[ADF702X_ARGV_DEV])->dev;
+	const struct ieee802154_radio_api *api = dev->api;
+	struct ieee802154_attr_value val;
+
+	api->attr_get(dev, 0, &val);
+
+	return 0;
+}
+
+static int cmd_adf702x_cw(const struct shell *sh, size_t argc, char **argv)
+{
+	const struct device *dev = get_adf702x(argv[ADF702X_ARGV_DEV])->dev;
+	const struct ieee802154_radio_api *api = dev->api;
+	const char *mode = argv[ADF702X_ARGV_CW_MODE];
+
+	if (argc < ADF702X_ARGV_CW_MODE) {
+		return -EIO;
+	}
+
+	if (!strcmp(mode, "start")) {
+		api->continuous_carrier(dev);
+	} else if (!strcmp(mode, "stop")) {
+		api->stop(dev);
+		api->start(dev);
+	} else {
+		shell_error(sh, "unknown command: %s, use <start|stop>", mode);
+	}
+
+	return 0;
+}
+
+static int cmd_adf702x_dump(const struct shell *sh, size_t argc, char **argv)
+{
+	const struct device *dev = get_adf702x(argv[ADF702X_ARGV_DEV])->dev;
+	const struct ieee802154_radio_api *api = dev->api;
+	struct ieee802154_attr_value val;
+
+	api->attr_get(dev, 2, &val);
+
+	return 0;
+}
+
+static int cmd_adf702x_tx(const struct shell *sh, size_t argc, char **argv)
+{
+#if CONFIG_IEEE802154_RAW_MODE
+	const struct device *dev = get_adf702x(argv[ADF702X_ARGV_DEV])->dev;
+	const struct ieee802154_radio_api *api = dev->api;
+	struct net_pkt *pkt;
+	struct net_buf *buf;
+	int bytes_to_send;
+	int ret = 0;
+
+	bytes_to_send = argc - ADF702X_ARGV_TX_FIRST;
+
+	if (bytes_to_send < 1) {
+		shell_error(sh, "No data to send: %d bytes...", bytes_to_send);
+		return -EINVAL;
+	}
+
+	if (bytes_to_send > ADF702X_MAX_TXLEN) {
+		shell_error(sh, "can't send more than %d bytes...", ADF702X_MAX_TXLEN);
+		return -EINVAL;
+	}
+
+	pkt = net_pkt_alloc_with_buffer(NULL, ADF702X_MAX_TXLEN, AF_UNSPEC, 0, K_NO_WAIT);
+	if (!pkt) {
+		shell_error(sh, "Failed to allocate net_pkt");
+	}
+
+	buf = net_buf_frag_last(pkt->buffer);
+	for (int i = 0; i < bytes_to_send; i++) {
+		net_pkt_write_u8(pkt, (int)strtol(argv[ADF702X_ARGV_TX_FIRST + i], NULL, 16));
+	}
+
+	/* Transmit data through radio */
+	ret = api->tx(dev, IEEE802154_TX_MODE_DIRECT, pkt, buf);
+	if (ret) {
+		shell_error(sh, "Error transmit data: %d", ret);
+	}
+
+	net_pkt_unref(pkt);
+
+#else
+	shell_warn(sh, "Raw TX disabled");
+#endif
+
+	return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	adf702x_cmds, SHELL_CMD_ARG(status, &dsub_adf702x, "read status", cmd_adf702x_status, 2, 0),
+	SHELL_CMD_ARG(dump, &dsub_adf702x, "dump ADF702x registers", cmd_adf702x_dump, 2, 0),
+	SHELL_CMD_ARG(cw, &dsub_adf702x, "control continuous carrier mode", cmd_adf702x_cw, 3, 0),
+#if CONFIG_IEEE802154_RAW_MODE
+	SHELL_CMD_ARG(tx, &dsub_adf702x, "Send raw data", cmd_adf702x_tx, 3, 250),
+#endif
+	SHELL_SUBCMD_SET_END);
+
+SHELL_CMD_REGISTER(adf702x, &adf702x_cmds, "ADF702x debug commands", NULL);
--- samples/lvb/pico_adf7024/src/main.c
+++ samples/lvb/pico_adf7024/src/main.c
@@ -21,19 +21,16 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
 /* ieee802.15.4 devices */
-static struct ieee802154_radio_api *rx_api;
 static const struct device *const rx_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_ieee802154_rx));
-
-static struct ieee802154_radio_api *tx_api;
 static const struct device *const tx_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_ieee802154_tx));
-
 
 static int init_led(void)
 {
 	int flags = 0;
 
-	if (!gpio_is_ready_dt(&led))
+	if (!gpio_is_ready_dt(&led)) {
 		return -ENODEV;
+	}
 
 	flags |= GPIO_OUTPUT | GPIO_OUTPUT_INIT_LOGICAL | GPIO_OUTPUT_INIT_HIGH;
 	return gpio_pin_configure_dt(&led, flags);
@@ -41,7 +38,7 @@ static int init_led(void)
 
 static bool init_ieee802154(void)
 {
-	struct ieee802154_attr_value val;
+	struct ieee802154_radio_api *tx_api, *rx_api;
 
 	/* Initialize ieee802154 device */
 	LOG_INF("Initialize ieee802.15.4 devices");
@@ -68,190 +65,17 @@ static bool init_ieee802154(void)
 	return 0;
 }
 
-static int cmd_adf702x_cw(const struct shell *sh, size_t argc, char **argv)
-{
-	if (argc < 2)
-		return -EIO;
-
-	if (!strcmp(argv[1], "start"))
-		tx_api->continuous_carrier(tx_dev);
-	else if (!strcmp(argv[1], "stop")) {
-		tx_api->stop(tx_dev);
-		tx_api->start(tx_dev);
-	} else
-		shell_error(sh, "unknown command: %s", argv[1]);
-
-	return 0;
-}
-
-static int cmd_adf702x_status(const struct shell *sh, size_t argc, char **argv)
-{
-	struct ieee802154_attr_value val;
-
-	rx_api->attr_get(rx_dev, 0, &val);
-	tx_api->attr_get(tx_dev, 0, &val);
-
-	return 0;
-}
-
-static int cmd_adf702x_get(const struct shell *sh, size_t argc, char **argv)
-{
-	struct ieee802154_attr_value val;
-
-	val.phy_supported_channel_pages = strtol(argv[1], NULL, 0);
-	LOG_INF("ADDR: 0x%x", val.phy_supported_channel_pages);
-
-	tx_api->attr_get(tx_dev, 1, &val);
-	rx_api->attr_get(rx_dev, 1, &val);
-
-	return 0;
-}
-
-static int cmd_adf702x_dump(const struct shell *sh, size_t argc, char **argv)
-{
-	struct ieee802154_attr_value val;
-
-	tx_api->attr_get(tx_dev, 2, &val);
-	rx_api->attr_get(rx_dev, 2, &val);
-
-	return 0;
-}
-
-static int cmd_adf702x_rx(const struct shell *sh, size_t argc, char **argv)
-{
-	struct ieee802154_attr_value val;
-
-	rx_api->attr_get(rx_dev, 3, &val);
-
-	return 0;
-}
-
-static int cmd_adf702x_tx(const struct shell *sh, size_t argc, char **argv)
-{
-#if CONFIG_IEEE802154_RAW_MODE
-	struct net_pkt *pkt;
-	struct net_buf *buf;
-	int ret = 0;
-
-	pkt = net_pkt_alloc_with_buffer(NULL, 100, AF_UNSPEC, 0, K_NO_WAIT);
-	if (!pkt)
-		LOG_ERR("Failed to allocate net_pkt");
-
-	buf = net_buf_frag_last(pkt->buffer);
-
-	int bytes_to_send = argc - 1;
-	if (bytes_to_send > 100) {
-		LOG_ERR("can't send that much data...");
-		goto out;
-	}
-
-	for (int i = 0; i < bytes_to_send; i++) {
-		net_pkt_write_u8(pkt, strtol(argv[1 + i], NULL, 16));
-	}
-
-	/* Transmit data through radio */
-	ret = tx_api->tx(tx_dev, IEEE802154_TX_MODE_DIRECT, pkt, buf);
-	if (ret) {
-		LOG_ERR("Error transmit data: %d", ret);
-	}
-
-out:
-	net_pkt_unref(pkt);
-#else
-	LOG_ERR("tx disabled, use iface");
-#endif
-
-	return 0;
-}
-
-#define TEST_PAYLOAD "TEST PAYLOAD"
-static int cmd_adf702x_iface(const struct shell *sh, size_t argc, char **argv)
-{
-	int ret;
-	struct net_pkt *tx_pkt;
-	struct net_if *iface = net_if_get_first_by_type(
-			&NET_L2_GET_NAME(DUMMY));
-
-	/* zassert_not_null(net_if_l2(iface), "No L2 found"); */
-	if (!net_if_l2(iface))
-		LOG_ERR("No L2 found");
-
-	/* zassert_not_null(net_if_l2(iface)->send, "No send() found"); */
-	if (!net_if_l2(iface)->send)
-		LOG_ERR("No send() found");
-
-	tx_pkt = net_pkt_alloc_with_buffer(iface, 1,
-			AF_UNSPEC, 0, K_NO_WAIT);
-	/* zassert_not_null(tx_pkt, "Failed to allocate packet"); */
-	if (!tx_pkt)
-		LOG_ERR("failed to allocate packet");
-
-	ret = net_pkt_write(tx_pkt, TEST_PAYLOAD, sizeof(TEST_PAYLOAD));
-	/* zassert_equal(0, ret, "Failed to write payload"); */
-	if (!ret)
-		LOG_ERR("Failed to write payload");
-
-	ret = net_send_data(tx_pkt);
-	/* zassert_equal(0, ret, "Failed to process TX packet"); */
-	if (!ret)
-		LOG_ERR("Failed to process TX packet");
-	/* zassert_equal(tx_pkt, test_data.tx_pkt, "TX packet did not reach L2"); */
-
-	net_pkt_unref(tx_pkt);
-
-#if 0
-	struct net_if *iface = net_if_get_first_by_type(&NET_L2_GET_NAME(DUMMY));
-	struct net_pkt *tx_pkt;
-	int ret = 0;
-
-	LOG_ERR("lvb in %p", iface);
-	tx_pkt = net_pkt_alloc_with_buffer(iface, sizeof(TEST_PAYLOAD), AF_UNSPEC, 0, K_NO_WAIT);
-	LOG_ERR("lvb in %p", tx_pkt);
-
-	LOG_ERR("lvb before net_pkt_write");
-	ret = net_pkt_write(tx_pkt, TEST_PAYLOAD, sizeof(TEST_PAYLOAD));
-	LOG_ERR("lvb ret %d", ret);
-
-	ret = net_send_data(tx_pkt);
-	LOG_ERR("lvb ret %d", ret);
-	net_pkt_unref(tx_pkt);
-#endif
-
-	return ret;
-}
-
-
-SHELL_STATIC_SUBCMD_SET_CREATE(
-	adf702x_cmds,
-	SHELL_CMD_ARG(cw, NULL, "control continuous carrier mode\n", cmd_adf702x_cw, 2, 0),
-#if CONFIG_IEEE802154_RAW_MODE
-	SHELL_CMD_ARG(get, NULL, "read register\n", cmd_adf702x_get, 2, 0),
-	SHELL_CMD_ARG(dump, NULL, "dump register\n", cmd_adf702x_dump, 0, 0),
-	SHELL_CMD_ARG(rx, NULL, "rx\n", cmd_adf702x_rx, 1, 0),
-	SHELL_CMD_ARG(tx, NULL, "tx\n", cmd_adf702x_tx, 1, 20),
-#else
-	SHELL_CMD_ARG(status, NULL, "read status\n", cmd_adf702x_status, 0, 0),
-	SHELL_CMD_ARG(iface, NULL, "iface\n", cmd_adf702x_iface, 0, 0),
-#endif
-	SHELL_SUBCMD_SET_END
-);
-SHELL_CMD_REGISTER(adf702x, &adf702x_cmds, "ADF702x commands", NULL);
-
 int main(void)
 {
-	/* struct net_if *iface; */
-	/* iface = net_if_get_by_index(1); */
-
-	LOG_DBG("TEST");
+	int ret = 0;
 
 	init_led();
 
 #if CONFIG_IEEE802154_RAW_MODE
-
-	/* Initialize ieee802154 device */
-	if (init_ieee802154()) {
+	ret = init_ieee802154();
+	if (ret) {
 		LOG_ERR("Unable to initialize ieee802154");
-		return 0;
+		return ret;
 	}
 #endif
 
