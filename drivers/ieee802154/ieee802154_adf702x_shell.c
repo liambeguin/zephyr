e2b24d86f1c384ac8b0cf19815618277efcbb02a
Liam Beguin <liambeguin@gmail.com>
2024-12-10 19:53:31 -0500

    ieee802154: adf702x: shell: implement raw socket tx function
    Rework the tx function to use a raw socket. early wip..
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- drivers/ieee802154/ieee802154_adf702x_shell.c
+++ drivers/ieee802154/ieee802154_adf702x_shell.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <zephyr/net_buf.h>
+#include <zephyr/net/ethernet.h>
 #include <zephyr/net/ieee802154.h>
+#include <zephyr/net/ieee802154_mgmt.h>
 #include <zephyr/net/ieee802154_radio.h>
+#include <zephyr/net/net_pkt.h>
 #include <zephyr/shell/shell.h>
+#include <zephyr/net/socket.h>
 
 #include "ieee802154_adf702x.h"
 
@@ -109,9 +113,9 @@ static int cmd_adf702x_stop(const struct shell *sh, size_t argc, char **argv)
 }
 #endif
 
+#if CONFIG_IEEE802154_RAW_MODE
 static int cmd_adf702x_tx(const struct shell *sh, size_t argc, char **argv)
 {
-#if CONFIG_IEEE802154_RAW_MODE
 	const struct device *dev = get_adf702x(argv[ADF702X_ARGV_DEV])->dev;
 	const struct ieee802154_radio_api *api = dev->api;
 	struct net_pkt *pkt;
@@ -148,25 +152,76 @@ static int cmd_adf702x_tx(const struct shell *sh, size_t argc, char **argv)
 	}
 
 	net_pkt_unref(pkt);
+	return 0;
+}
 
 #else
-	shell_warn(sh, "Raw TX disabled");
-#endif
+static int cmd_adf702x_tx(const struct shell *sh, size_t argc, char **argv)
+{
+	const struct device *dev = get_adf702x(argv[ADF702X_ARGV_DEV])->dev;
+	struct net_if *iface = net_if_lookup_by_dev(dev);
+	int bytes_to_send;
+	uint8_t raw_payload[64] = {0};
+	int ret;
 
-	return 0;
+	struct sockaddr_ll socket_sll = {0};
+	struct msghdr msg = {0};
+	struct iovec io_vector;
+	bool result = false;
+	int fd;
+
+	bytes_to_send = argc - ADF702X_ARGV_TX_FIRST;
+
+	for (int i = 0; i < bytes_to_send; i++) {
+		raw_payload[i] = (uint8_t)strtol(argv[ADF702X_ARGV_TX_FIRST + i], NULL, 16);
+	}
+
+	ret = net_if_up(iface);
+	if (ret) {
+		shell_error(sh, "if up error %d", ret);
+	}
+
+	shell_info(sh, "- Sending RAW packet via AF_PACKET socket");
+
+	fd = zsock_socket(AF_PACKET, SOCK_RAW, htons(ETH_P_IEEE802154));
+	if (fd < 0) {
+		shell_error(sh, "*** Failed to create RAW socket : %d", errno);
+		goto out;
+	}
+
+	socket_sll.sll_ifindex = net_if_get_by_iface(iface);
+	socket_sll.sll_family = AF_PACKET;
+	socket_sll.sll_protocol = ETH_P_IEEE802154;
+
+	if (zsock_bind(fd, (const struct sockaddr *)&socket_sll, sizeof(struct sockaddr_ll))) {
+		shell_error(sh, "*** Failed to bind packet socket : %d", errno);
+		goto release_fd;
+	}
+
+	io_vector.iov_base = raw_payload;
+	io_vector.iov_len = bytes_to_send;
+	msg.msg_iov = &io_vector;
+	msg.msg_iovlen = 1;
+
+	if (zsock_sendmsg(fd, &msg, 0) != bytes_to_send) {
+		shell_error(sh, "*** Failed to send, errno %d", errno);
+		goto release_fd;
+	}
+
+release_fd:
+	zsock_close(fd);
+out:
+	return result;
+#endif
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(
 	adf702x_cmds, SHELL_CMD_ARG(status, &dsub_adf702x, "read status", cmd_adf702x_status, 2, 0),
-#ifndef CONFIG_IEEE802154_RAW_MODE
 	SHELL_CMD_ARG(start, &dsub_adf702x, "start ADF702x iface", cmd_adf702x_start, 2, 0),
 	SHELL_CMD_ARG(stop, &dsub_adf702x, "stop ADF702x iface", cmd_adf702x_stop, 2, 0),
-#endif
 	SHELL_CMD_ARG(dump, &dsub_adf702x, "dump ADF702x registers", cmd_adf702x_dump, 2, 0),
 	SHELL_CMD_ARG(cw, &dsub_adf702x, "control continuous carrier mode", cmd_adf702x_cw, 3, 0),
-#if CONFIG_IEEE802154_RAW_MODE
 	SHELL_CMD_ARG(tx, &dsub_adf702x, "Send raw data", cmd_adf702x_tx, 3, 250),
-#endif
 	SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(adf702x, &adf702x_cmds, "ADF702x debug commands", NULL);
