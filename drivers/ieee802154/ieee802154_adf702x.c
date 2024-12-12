721197d66f48ff5b044de1e30be4cacf2e887bc9
Liam Beguin <liambeguin@gmail.com>
2024-12-11 23:16:58 -0500

    ieee802154: adf702x: reduce log level for traces
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- drivers/ieee802154/ieee802154_adf702x.c
+++ drivers/ieee802154/ieee802154_adf702x.c
@@ -525,7 +525,7 @@ static int adf702x_set_txpower(const struct device *dev, int16_t dBm)
 {
 	const struct adf702x_config *conf = dev->config;
 
-	LOG_INST_ERR(conf->log, "%s: dBm=%d", __func__, dBm);
+	LOG_INST_DBG(conf->log, "%s: dBm=%d", __func__, dBm);
 
 	return adf702x_regs_set_pa_level(dev, dBm);
 }
@@ -539,7 +539,7 @@ static int adf702x_tx(const struct device *dev,
 	struct adf702x_context *ctx = dev->data;
 	int ret;
 
-	LOG_INST_ERR(conf->log, "%s: entering", __func__);
+	LOG_INST_DBG(conf->log, "%s: entering", __func__);
 
 	if (!ctx->is_up)
 		return -ENETDOWN;
