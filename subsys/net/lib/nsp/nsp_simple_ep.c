d1ccc96411c2c6665fbe579286519d197772a9c1
Liam Beguin <liambeguin@gmail.com>
2025-01-26 15:31:26 -0500

    net: nsp: simple_ep: return version on PING
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- subsys/net/lib/nsp/nsp_simple_ep.c
+++ subsys/net/lib/nsp/nsp_simple_ep.c
@@ -1,9 +1,12 @@
+#include <app_version.h>
 #include <zephyr/net/nsp.h>
 #include <zephyr/zbus/zbus.h>
 
 #include <zephyr/logging/log.h>
 #include <zephyr/net/nsp.h>
 
+#define xstr(s) str(s)
+#define str(s) #s
 #define NSP_SIMPLE_EP_ADDR 0x20
 
 LOG_MODULE_REGISTER(nsp_ep, LOG_LEVEL_DBG);
@@ -32,7 +35,7 @@ void simple_ep_cb(const struct zbus_channel *chan)
 
 	switch (rxpkt->cmd) {
 	case PING:
-		char *name = "Hello From Zephyr";
+		char *name = "Zephyr " xstr(APP_BUILD_VERSION);
 		txpkt.a = 1; // ACK
 		txpkt.payload = name;
 		txpkt.len = strlen(name);
