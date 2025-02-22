dced6d3370d6cb13efdc7bbf8641150fa846994d
Liam Beguin <liambeguin@gmail.com>
2025-02-22 18:57:15 -0500

    net: nsp: shell: implement PEEK and POKE
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- subsys/net/lib/nsp/nsp_shell.c
+++ subsys/net/lib/nsp/nsp_shell.c
@@ -7,6 +7,7 @@
 
 #define NSP_ARGV_EP     (1)
 #define NSP_ARGV_ADDR   (2)
+#define NSP_ARGV_LEN    (3)
 #define NSP_ARGV_VAL    (3)
 
 struct shell *shared_sh;
@@ -97,23 +98,53 @@ static int cmd_nsp_init(const struct shell *sh, size_t argc, char **argv)
 
 static int cmd_nsp_peek(const struct shell *sh, size_t argc, char **argv)
 {
-	shell_error(sh, "nsp peek");
+	NSP_PKT_DEFINE(txpkt);
+	uint32_t addr = 0;
+	uint16_t len = 0;
 
-	return 0;
+	shared_sh = (struct shell *)sh;
+
+	txpkt.src = CONFIG_NSP_SHELL_SRC_ADDR;
+	txpkt.dst = (int)strtol(argv[NSP_ARGV_EP], NULL, 16);
+	txpkt.cmdid = PEEK;
+	txpkt.pf = 1;
+
+	addr = strtol(argv[NSP_ARGV_ADDR], NULL, 16);
+	net_buf_add_le32(txpkt.buf, addr);
+
+	len = strtol(argv[NSP_ARGV_LEN], NULL, 16);
+	net_buf_add_le16(txpkt.buf, len);
+
+	return nsp_send(&txpkt);
 }
 
 static int cmd_nsp_poke(const struct shell *sh, size_t argc, char **argv)
 {
-	shell_error(sh, "nsp poke");
+	NSP_PKT_DEFINE(txpkt);
+	uint32_t addr = 0;
+	uint32_t value = 0;
 
-	return 0;
+	shared_sh = (struct shell *)sh;
+
+	txpkt.src = CONFIG_NSP_SHELL_SRC_ADDR;
+	txpkt.dst = (int)strtol(argv[NSP_ARGV_EP], NULL, 16);
+	txpkt.cmdid = POKE;
+	txpkt.pf = 1;
+
+	addr = strtol(argv[NSP_ARGV_ADDR], NULL, 16);
+	net_buf_add_le32(txpkt.buf, addr);
+
+	value = strtol(argv[NSP_ARGV_VAL], NULL, 16);
+	net_buf_add_le32(txpkt.buf, value);
+
+	return nsp_send(&txpkt);
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(
 	nsp_cmds,
 	SHELL_CMD_ARG(ping, NULL, "nsp ping <ep>", cmd_nsp_ping, 2, 0),
 	SHELL_CMD_ARG(init, NULL, "nsp init <ep> [addr]", cmd_nsp_init, 2, 1),
-	SHELL_CMD_ARG(peek, NULL, "nsp peek <ep> <addr>", cmd_nsp_peek, 3, 0),
+	SHELL_CMD_ARG(peek, NULL, "nsp peek <ep> <addr> <len>", cmd_nsp_peek, 4, 0),
 	SHELL_CMD_ARG(poke, NULL, "nsp poke <ep> <addr> <value>", cmd_nsp_poke, 4, 0),
 	SHELL_SUBCMD_SET_END);
 
